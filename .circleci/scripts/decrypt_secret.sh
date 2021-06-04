e493eabf596ff797871181b7eae2b81b440a1572
YesicaAcha <yesica.7887@gmail.com>
2021-06-04 11:55:20 -0400

    update files

--- .circleci/scripts/decrypt_secret.sh
+++ .circleci/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output gradle.properties gradle.properties.gpg
+--output gradle.properties gradle.properties.gpg 
